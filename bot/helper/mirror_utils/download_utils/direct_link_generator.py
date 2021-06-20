abdc473ab5c4fc297579a3b33cbb36b2909722cf
mornflakes928 <77254405+mornflakes928@users.noreply.github.com>
2021-06-20 13:20:32 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -43,7 +43,7 @@ def direct_link_generator(link: str):
     elif 'mediafire.com' in link:
         return mediafire(link)
     elif 'uptobox.com' in link:
-        return uptobox(link)
+        raise DirectDownloadLinkException(f"ERROR: Mohon maaf, Uptobox tidak support karena admin belum mempunyai key. Silahkan berdonasi key UTB agar bisa digunakan.")
     elif 'osdn.net' in link:
         return osdn(link)
     elif 'github.com' in link:
